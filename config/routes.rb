4f0f26c3a8accc4692a7c84818e890c79deb634d
sadaking <sichirigahama.37@gmail.com>
2020-07-23 17:32:33 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'products#index'
   resources :products
+  devise_for :producers, controllers: {
+    sessions:      'producers/sessions',
+    passwords:     'producers/passwords',
+    registrations: 'producers/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  resources :products
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
