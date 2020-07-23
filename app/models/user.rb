566c0ad97a822c51bf26ae0d7ebbe7d59a9923ab
sadaking <sichirigahama.37@gmail.com>
2020-07-23 17:29:18 +0900

    ユーザー用の認証システム作成(仮)

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+   include JpPrefecture
+   jp_prefecture :prefecture_code, method_name: :pref
+
+
+   def prefecture_name
+     JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+   end
+
+   def prefecture_name=(prefecture_name)
+     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+   end
+end
