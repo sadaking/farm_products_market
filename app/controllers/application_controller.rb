794a5283f7766ae9ad38f6700ca4c797dd5c90b5
sadaking <sichirigahama.37@gmail.com>
2020-07-25 15:24:11 +0900

    /application_controller.rbの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,24 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,
+    if resource_class == User
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,
                                                         :postal_code,
                                                         :prefecture_code,
                                                         :address_city,
                                                         :address_street,
                                                         :address_building,
                                                         :admin])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:producer_name,
+    elsif resource_class == Producer
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:producer_name,
                                                         :producer_postal_code,
                                                         :producer_prefecture_code,
                                                         :producer_address_city,
                                                         :producer_address_street,
                                                         :producer_address_building,
                                                         :admin])
+    else
+      super
+    end
   end
 end
