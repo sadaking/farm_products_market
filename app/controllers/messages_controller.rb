a201875a25922203fb2eea4c297b015b3bbbd96d
sadaking <sichirigahama.37@gmail.com>
2020-07-25 15:33:43 +0900

    messagesコントローラの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+  def create
+    @room = Room.find(params[:room_id])
+    @message = Message.new(message_params)
+    if user_signed_in?
+      @message.is_user = true
+    elsif producer_signed_in?
+      @message.is_user = false
+    end
+    @message.room_id = @room.id
+    if @message.save
+      redirect_to room_url(@room)
+    else
+      redirect_to room_url(@room)
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
