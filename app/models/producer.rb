646b9a0d06e4d59d22b2ed4e3928f6d1f1119ed7
sadaking <sichirigahama.37@gmail.com>
2020-07-27 15:09:11 +0900

    変更なし

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -5,5 +5,4 @@ class Producer < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :rooms
-  scope :title_search, -> (title) {where("title LIKE ?", "%#{title}%")}
 end
