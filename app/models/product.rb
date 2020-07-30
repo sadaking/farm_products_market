6ddfa0254817ef6b1fb2d458a6dbef538e58482d
sadaking <sichirigahama.37@gmail.com>
2020-07-30 22:41:54 +0900

    多対多のアソシエーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   has_many :labelings, dependent: :destroy
   has_many :labels, through: :labelings
   has_many :comments, dependent: :destroy
