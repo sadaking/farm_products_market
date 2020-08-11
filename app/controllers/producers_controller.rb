11099262faaaeb87d32be84f32508a6d234fe1ad
sadaking <sichirigahama.37@gmail.com>
2020-08-11 16:24:52 +0900

    プロフィールページを他の人から閲覧されないようにする

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,5 +1,8 @@
 class ProducersController < ApplicationController
   def show
     @producer = Producer.find(params[:id])
+    unless @producer == current_producer
+      redirect_to products_path, notice: '他の人のプロフィール画面へはアクセスが出来ません。'
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to products_path, notice: '他の人のプロフィール画面へはアクセスが出来ません。'
+    end
   end
 end
