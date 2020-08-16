f5f1f0f1793b9ac41626f982a4de2fdf705f5ddc
sadaking <sichirigahama.37@gmail.com>
2020-08-16 20:36:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   resources :products
   resources :products do
     resources :comments
-    post :pay, on: :member
+    resources :purchases, only: [:index] do
+      collection do
+        get 'index', to: 'purchases#index'
+        post 'pay', to: 'purchases#pay'
+        get 'done', to: 'purchases#done'
+      end
+    end
   end
   resources :favorites, only: %i[create destroy index]
   resources :message_tops, only: %i[index]
@@ -14,7 +20,7 @@ Rails.application.routes.draw do
     registrations: 'producers/registrations'
   }
   resources :producers, only: [:show]
-  
+
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
@@ -30,5 +36,13 @@ Rails.application.routes.draw do
 
   resources :rooms, :only => [:show, :create] do
     resources :messages, :only => [:create]
+  end
+
+  resources :cards, only: [:new, :show] do
+    collection do
+      post 'show', to: 'cards#show'
+      post 'pay', to: 'cards#pay'
+      post 'delete', to: 'cards#delete'
+    end
   end
 end
