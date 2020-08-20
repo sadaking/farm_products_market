1985595bcbed360c6a1dc4309af68c7f2a66b7cf
sadaking <sichirigahama.37@gmail.com>
2020-08-20 11:04:52 +0900

    seedデータの修正

--- db/fixtures/production/02_product.rb
+++ db/fixtures/production/02_product.rb
@@ -5,7 +5,7 @@ Product.seed(
   { id: 9, title: 'さくらんぼの詰め合わせ', price: 2100, quantity: 2, content: '山形といえばさくらんぼ！徹底した管理のもとで育ったうちの商品は何処にも負けません。', harvest_date: '2020-08-15', farm_name: '米沢農場', farm_street_address: '山形県米沢市金池5-2-25', producer_id: 6 },
   { id: 10, title: '野菜詰め合わせ', price: 2200, quantity: 1, content: '北の大地で育った野菜はとても水々しいです！！', harvest_date: '2020-08-10', farm_name: '旭川農園', farm_street_address: '北海道旭川市6条通り6-9', producer_id: 7 },
   { id: 11, title: 'マンゴー詰め合わせ', price: 1700, quantity: 3, content: '宮崎のマンゴーには負けん！', harvest_date: '2020-08-16', farm_name: '南さつま園', farm_street_address: '鹿児島県南さつま市加世田川畑2648', producer_id: 8 },
-  { id: 12, title: 'フルーツ詰め合わせ', price: 2100', quantity: 1, content: '瀬戸内の温暖な機構で育った甘い甘いフルーツたちです！', harvest_date: '2020-08-14', farm_name: '西条農園', farm_street_address: '愛媛県西条市明屋敷1-6-4', producer_id: 9 },
+  { id: 12, title: '野菜詰め合わせ', price: 2200, quantity: 1, content: '北の大地で育った野菜はとても水々しいです！！', harvest_date: '2020-08-10', farm_name: '旭川農園', farm_street_address: '北海道旭川市6条通り6-9', producer_id: 9 },
   { id: 13, title: 'フルーツ詰め合わせ', price: 1900, quantity: 2, content: '江田島ブランドとしてメディアに取り上げられています！', harvest_date: '2020-08-17', farm_name: '江田島農場', farm_street_address: '広島県江田島市大柿町大原505', producer_id: 4 },
   { id: 14, title: '甲州ぶどうの詰め合わせ', price: 1700, quantity: 3, content: 'やっぱり葡萄といえば山梨の葡萄でしょ！！', harvest_date: '2020-08-14', farm_name: '勝沼フルーツ園', farm_street_address: '山梨県甲州市塩山上於曽1085-1', producer_id: 5 },
   { id: 15, title: 'さくらんぼの詰め合わせ', price: 2300', quantity: 4, content: 'フルーツ王国山形で地元からの支持が厚い農場のフルーツです！！', harvest_date: '2020-08-13', farm_name: '米沢農場', farm_street_address: '山形県米沢市金池5-2-25', producer_id: 6 },
@@ -18,7 +18,7 @@ Product.seed(
   { id: 22, title: '野菜詰め合わせ', price: 1600, quantity: 2, content: '旭川ブランドとしてメディアに取り上げられています！', harvest_date: '2020-08-08', farm_name: '旭川農園', farm_street_address: '北海道旭川市6条通り6-9', producer_id: 7 },
   { id: 23, title: 'マンゴー詰め合わせ', price: 2000, quantity: 1, content: '無農薬で育てているため、甘さが際立っています！', harvest_date: '2020-08-06', farm_name: '南さつま園', farm_street_address: '鹿児島県南さつま市加世田川畑2648', producer_id: 8 },
   { id: 24, title: 'フルーツ詰め合わせ', price: 2300, quantity: 2, content: '丹精込めて作ったフルーツです！ご賞味あれ！！', harvest_date: '2020-08-07', farm_name: '西条農園', farm_street_address: '愛媛県西条市明屋敷1-6-4', producer_id: 9 },
-  { id: 25, title: 'フルーツ詰め合わせ', price: 2100, quantity: 3, content: '瀬戸内の温暖な機構で育った甘い甘いフルーツたちです！', harvest_date: '2020-08-11', farm_name: '江田島農場', farm_street_address: '広島県江田島市大柿町大原505', producer_id: 4 },
+  { id: 25, title: 'さくらんぼの詰め合わせ', price: 2100, quantity: 2, content: '山形といえばさくらんぼ！徹底した管理のもとで育ったうちの商品は何処にも負けません。', harvest_date: '2020-08-15', farm_name: '米沢農場', farm_street_address: '山形県米沢市金池5-2-25', producer_id: 4 },
   { id: 26, title: '甲州ぶどうの詰め合わせ', price: 900, quantity: 4, content: '特殊な土地の形状が勝沼葡萄を甘く仕上げています！！', harvest_date: '2020-08-13', farm_name: '勝沼フルーツ園', farm_street_address: '山梨県甲州市塩山上於曽1085-1', producer_id: 5 },
   { id: 27, title: 'さくらんぼの詰め合わせ', price: 1200, quantity: 2, content: '山形といえばさくらんぼ！徹底した管理のもとで育ったうちの商品は何処にも負けません。', harvest_date: '2020-08-16', farm_name: '米沢農場', farm_street_address: '山形県米沢市金池5-2-25', producer_id: 6 },
 )
