a4894f0472da2e15db222bffe644d478ceeb4806
sadaking <sichirigahama.37@gmail.com>
2020-10-13 16:34:22 +0900

    ブログ機能のコントローラー関連

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,46 @@
+class BlogsController < ApplicationController
+  before_action :authenticate_producer!
+  before_action :set_blog, only: %i(edit update destroy)
+
+  def index
+    @blogs = Blog.order(id: :desc)
+  end
+
+  def new
+    @blog = current_producer.blogs.build
+  end
+
+  def create
+    @blog = current_producer.blogs.build(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "Successfully saved."
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: 'Successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: 'Successfully destroyed.'
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit %i(title content)
+  end
+end
